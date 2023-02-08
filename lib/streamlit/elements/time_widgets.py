15a99bc3cb55c12518f4d59ac82daefeec2338ea
Tomek Szerszeń <tomasz.szerszen@snowflake.com>
2023-02-08 09:13:00 +0100

    add instance check

--- lib/streamlit/elements/time_widgets.py
+++ lib/streamlit/elements/time_widgets.py
@@ -356,6 +356,10 @@ class TimeWidgetsMixin:
         time_input_proto.label = label
         time_input_proto.default = time.strftime(parsed_time, "%H:%M")
         time_input_proto.form_id = current_form_id(self.dg)
+        if not isinstance(step, (int, timedelta)):
+            raise StreamlitAPIException(
+                f"`step` can only be `int` or `timedelta` but {type(step)} is provided."
+            )
         if isinstance(step, timedelta):
             step = step.seconds
         if step < 60 or step > timedelta(hours=23).seconds:
